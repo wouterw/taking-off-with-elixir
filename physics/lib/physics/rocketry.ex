c74c8baae4011e430cf4038615244439ea3a5c5a
Wouter Willaert <wouter.willaert@gmail.com>
2017-01-27 19:31:36 +0100

    wip

--- physics/lib/physics/rocketry.ex
+++ physics/lib/physics/rocketry.ex
@@ -1,40 +1,45 @@
-defmodule Converter do
-  def to_nearest_tenth(value) do
-    Float.ceil(value, 1)
-  end
-
-  def to_km(velocity) do
-    velocity / 1000
-  end
-
-  def to_light_seconds({:miles, miles}) do
-    (miles * 5.36819e-6) |> round_down
-  end
-
-  def to_light_seconds({:meters, meters}) do
-    (meters * 3.335638620368e-9) |> round_down
-  end
-
-  def round_down(value) when is_float(value), do: trunc(value)
-end
-
 defmodule Physics.Rocketry do
-  def escape_velocity(:earth) do
-    %{mass: 5.972e24, radius: 6.371e6} |> escape_velocity
-  end
+  import Converter
+  import Calcs
+  import Physics.Laws
+  import Planets
 
   # Lowest velocity which a body must have in order to escape
   # the gravitational attraction of a particular planet or other object.
+
+  def escape_velocity(:moon), do: moon |> escape_velocity
+  def escape_velocity(:mars), do: mars |> escape_velocity
+  def escape_velocity(:earth), do: earth |> escape_velocity
+
   def escape_velocity(planet) when is_map(planet) do
     planet
       |> calculate_escape
-      |> Converter.to_km
-      |> Converter.to_nearest_tenth
+      |> to_km
+      |> to_nearest_tenth
+  end
+
+  def orbital_acceleration(height) do
+    (orbital_speed(height) |> squared) / orbital_radius(height)
+  end
+
+  def orbital_term(height) do
+    4 * (:math.pi |> squared) * (orbital_radius(height) |> cubed) /
+      (newtons_gravitational_constant * earth.mass)
+      |> square_root
+      |> seconds_to_hours
+  end
+
+  def orbital_speed(height) do
+    newtons_gravitational_constant * earth.mass / orbital_radius(height)
+      |> square_root
+  end
+
+  defp orbital_radius(height) do
+    earth.radius + (height |> to_meters)
   end
 
   defp calculate_escape(%{mass: mass, radius: radius}) do
-    newtons_constant = 6.67e-11
-    2 * newtons_constant * mass / radius
-      |> :math.sqrt
+    2 * newtons_gravitational_constant * mass / radius
+      |> square_root
   end
 end
